1f5c0f4eca21c989e1bde8f1951c0a0e56ad128c
DenizUzundag <uzundagdeniz@gmail.com>
2021-03-24 02:47:01 +0300

    Startup ayarlarının yapılandırılması, Admin ve Member Areaların eklenmesi, Aciliyetlerin listelenmesi,eklenmesi ve düzenlenmesi işlemleri

--- YSKProje.ToDo.Web/Controllers/HomeController.cs
+++ YSKProje.ToDo.Web/Controllers/HomeController.cs
@@ -2,12 +2,17 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
-
+using YSKProje.ToDo.Business.Interfaces;
 
 namespace YSKProje.ToDo.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IGorevService _gorevService;
+        public HomeController(IGorevService gorevService)
+        {
+            _gorevService = gorevService;
+        }
         public IActionResult Index()
         {
             return View();
--- YSKProje.ToDo.Web/Startup.cs
+++ YSKProje.ToDo.Web/Startup.cs
@@ -2,6 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using YSKProje.ToDo.Business.Concrete;
+using YSKProje.ToDo.Business.Interfaces;
+using YSKProje.ToDo.DataAccess.Concrete.EntityFrameworkCore.Context;
+using YSKProje.ToDo.DataAccess.Concrete.EntityFrameworkCore.Repositories;
+using YSKProje.ToDo.DataAccess.Interfaces;
+using YSKProje.ToDo.Entities.Concrete;
 
 namespace YSKProje.ToDo.Web
 {
@@ -11,6 +17,18 @@ namespace YSKProje.ToDo.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //addScoped :bir tane örnek alýr. ilgili interface ait bir örnek alýr ve her zaman onu veriri
+            //AddTransient: her seferinde yeni bir nesne örneði veriri
+            //AddSingleton: istek kimden gelmiþ nerden gelmiþ önemi yok tek bir nesne örneði üretir.
+            services.AddScoped<IGorevService, GorevManager>();//gorevServiceyi görünce gorevManageri örnekle
+            services.AddScoped<IAciliyetService, AciliyetManager>();
+            services.AddScoped<IRaporService, RaporManager>();
+            services.AddScoped<IGorevDal, EfGorevRepository>();
+            services.AddScoped<IRaporDal, EfRaporRepository>();
+            services.AddScoped<IAciliyetDal, EfAciliyetRepository>();
+
+            services.AddDbContext<TodoContext>();
+            services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<TodoContext>();
             services.AddControllersWithViews();
         }
 
@@ -27,7 +45,17 @@ namespace YSKProje.ToDo.Web
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapDefaultControllerRoute();
+                endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area}/{controller=Home}/{action=Index}/{id?}"
+
+                    );
+
+
+                endpoints.MapControllerRoute(
+                    name:"defaukt",
+                    pattern:"{controller=Home}/{action=Index}/{id?}"
+                    );
             });
         }
     }
