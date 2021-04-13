bfbf6f5b090f082a02634ddf2560b0722d35ae90
DenizUzundag <uzundagdeniz@gmail.com>
2021-04-14 01:10:42 +0300

    Profil Güncelleme İşleminin Yapılması

--- YSKProje.ToDo.Web/Areas/Admin/Controllers/ProfilController.cs
+++ YSKProje.ToDo.Web/Areas/Admin/Controllers/ProfilController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using YSKProje.ToDo.Entities.Concrete;
@@ -30,6 +32,46 @@ namespace YSKProje.ToDo.Web.Areas.Admin.Controllers
             model.SurName = appuser.Surname;
             model.Picture = appuser.Picture;
             return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(AppUSerListViewModel model,IFormFile resim)
+        {
+            if (ModelState.IsValid)
+            {
+                var guncellenecekKullanici =  _userManager.Users.FirstOrDefault(I => I.Id == model.Id);
+                if(resim != null)
+                {
+                    string uzanti = Path.GetExtension(resim.FileName);
+                    string resimAd = Guid.NewGuid() + uzanti;
+
+                    string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/" + resimAd);
+
+                    using (var stream=new FileStream(path,FileMode.Create))
+                    {
+                       await resim.CopyToAsync(stream);
+                    }
+                    guncellenecekKullanici.Picture = resimAd;
+                    guncellenecekKullanici.Name = model.Name;
+                    guncellenecekKullanici.Surname = model.SurName;
+                    guncellenecekKullanici.Email = model.Email;
+
+                  var result = await _userManager.UpdateAsync(guncellenecekKullanici);
+                    if(result.Succeeded)
+                    {
+                        TempData["Message"] = "Güncelleme işleminiz başarı ile gerçekleşti";
+                        return RedirectToAction("Index");
+                    }
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            
+            
+            }
+            return View(model);
+     
         }
     }
 }
