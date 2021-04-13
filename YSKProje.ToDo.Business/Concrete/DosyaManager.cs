77b0709d04f9980ff1e46feb92ca39a1a962d5a1
DenizUzundag <uzundagdeniz@gmail.com>
2021-04-13 21:31:58 +0300

    Excel ve Pdf üreten Manager Interface ve sınıfları oluşturduk.

---
+++ YSKProje.ToDo.Business/Concrete/DosyaManager.cs
@@ -0,0 +1,64 @@
+﻿using FastMember;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Text;
+using YSKProje.ToDo.Business.Interfaces;
+
+namespace YSKProje.ToDo.Business.Concrete
+{
+    public class DosyaManager : IDosyaManager
+    {
+        public byte[] AktarExcel<T>(List<T> list) where T : class, new()
+        {
+            var excelPackage = new ExcelPackage();
+            var excelBlank = excelPackage.Workbook.Worksheets.Add("Calisma1");
+            excelBlank.Cells["A1"].LoadFromCollection(list, true, OfficeOpenXml.Table.TableStyles.Light15);//liste dönücek headerları göster style light15
+            return excelPackage.GetAsByteArray();
+            
+        }
+
+        public string AktarPdf<T>(List<T> list) where T : class, new()
+        {
+            DataTable dataTable = new DataTable();
+            dataTable.Load(ObjectReader.Create(list));
+
+
+
+            var filename = Guid.NewGuid() + ".pdf";
+            var returnPath = "/documents/"+filename;
+            var path = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/documents/"+filename);
+
+            var stream = new FileStream("", FileMode.Create);
+
+            Document document = new Document(PageSize.A4,25f,25f,25f,25f);
+            //documenti pdfe aktarmak için..
+            PdfWriter.GetInstance(document, null);
+            document.Open();
+
+            PdfPTable pdfPTable = new PdfPTable(dataTable.Columns.Count);
+            for (int i = 0; i < dataTable.Columns.Count; i++)
+            {
+                pdfPTable.AddCell(dataTable.Columns[i].ColumnName);
+            }
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                for (int j = 0; j < dataTable.Columns.Count; j++)
+                {
+                    pdfPTable.AddCell(dataTable.Rows[i][j].ToString());
+                }
+            }
+            document.Add(pdfPTable);
+            document.Close();
+
+            return returnPath;
+
+        }
+
+
+    }
+}
---
+++ YSKProje.ToDo.Business/Interfaces/IDosyaManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YSKProje.ToDo.Business.Interfaces
+{
+    public interface IDosyaManager
+    {
+        /// <summary>
+        /// Geriye üretmiş ve upload etmiş olduğu pdf dosyasının virtual pathini döner
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        string AktarPdf<T>(List<T> list) where T: class,new();
+        /// <summary>
+        /// Geriye Excel verisinin byte dizisi olarak döner
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        byte[] AktarExcel<T>(List<T> list) where T : class, new();
+    }
+}
