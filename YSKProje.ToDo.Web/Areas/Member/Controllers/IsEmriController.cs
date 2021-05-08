7d7dd7de8274957bd0bdceba16da92708f1111bb
DenizUzundag <uzundagdeniz@gmail.com>
2021-05-08 23:47:43 +0300

    Rapor Ekleme işlemi gerçekleştirildi

--- YSKProje.ToDo.Web/Areas/Member/Controllers/IsEmriController.cs
+++ YSKProje.ToDo.Web/Areas/Member/Controllers/IsEmriController.cs
@@ -58,5 +58,23 @@ namespace YSKProje.ToDo.Web.Areas.Member.Controllers
             model.Gorev = gorev;
             return View(model);
         }
+
+        [HttpPost]
+        public IActionResult EkleRapor(RaporAddViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                _raporService.Kaydet(new Rapor()
+                {
+                    GorevId = model.GorevId,
+                    Detay = model.Detay,
+                    Tanim =model.Tanim
+                }) ;
+                return RedirectToAction("Index");
+
+            }
+            return View(model);
+
+        }
     }
 }
