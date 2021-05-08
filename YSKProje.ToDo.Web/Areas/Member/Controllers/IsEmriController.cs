fa8e2373e3ecc547b587b8c9f25489455b64a8b9
DenizUzundag <uzundagdeniz@gmail.com>
2021-05-08 23:41:06 +0300

    Raporların Collapse aracılığıyla gösterilmesi, Rapor ekleme formunun tasarlanması

--- YSKProje.ToDo.Web/Areas/Member/Controllers/IsEmriController.cs
+++ YSKProje.ToDo.Web/Areas/Member/Controllers/IsEmriController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using YSKProje.ToDo.Business.Interfaces;
+using YSKProje.ToDo.Entities.Concrete;
 using YSKProje.ToDo.Web.Areas.Admin.Models;
 
 namespace YSKProje.ToDo.Web.Areas.Member.Controllers
@@ -11,16 +13,21 @@ namespace YSKProje.ToDo.Web.Areas.Member.Controllers
     [Area("Member")]
     public class IsEmriController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
         private readonly IGorevService _gorevService;
+        private readonly IRaporService _raporService;
 
-        public IsEmriController(IGorevService gorevService)
+        public IsEmriController(IGorevService gorevService ,IRaporService raporService, UserManager<AppUser> userManager)
         {
             _gorevService = gorevService;
+            _userManager = userManager;
+            _raporService = raporService;
         }
-        public IActionResult Index(int id)
+        public async Task<IActionResult> Index()
         {
+           var user =  await _userManager.FindByNameAsync(User.Identity.Name);
             TempData["Active"] = "isemri";
-            var gorevler= _gorevService.GetirTumTablolarla(I => I.AppUserId == id && !I.Durum);
+            var gorevler= _gorevService.GetirTumTablolarla(I => I.AppUserId == user.Id && !I.Durum);
 
             List<GorevListAllViewModel> models  = new List<GorevListAllViewModel>();
 
@@ -40,6 +47,16 @@ namespace YSKProje.ToDo.Web.Areas.Member.Controllers
             }
           
             return View(models);
+        }
+        public IActionResult EkleRapor(int id)
+        {
+            TempData["Active"] = "isemri";
+           var gorev= _gorevService.GetirAciliyetileId(id);
+
+            RaporAddViewModel model = new RaporAddViewModel();
+            model.GorevId = id;
+            model.Gorev = gorev;
+            return View(model);
         }
     }
 }
