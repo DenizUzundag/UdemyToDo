82aecde0b90a704789cd9b8c3fffefb6df5ed393
DenizUzundag <uzundagdeniz@gmail.com>
2021-05-25 01:36:24 +0300

    Fluent Validation'ın Projeye Eklenmesi

---
+++ YSKProje.ToDo.Business/ValidationRules/FluentValidation/AciliyetUpdateValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using YSKProje.ToDo.DTO.DTOs.AciliyetDtos;
+
+namespace YSKProje.ToDo.Business.ValidationRules.FluentValidation
+{
+    public class AciliyetUpdateValidatorAccessViolationException: AbstractValidator<AciliyetUpdateDto>
+    {
+        public AciliyetUpdateValidatorAccessViolationException()
+        {
+            RuleFor(I => I.Tanim).NotNull().WithMessage("Tanım alanı boş geçilemez");
+        }
+    }
+}
