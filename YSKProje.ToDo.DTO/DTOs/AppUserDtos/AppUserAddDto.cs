017b012662907777992f5eddd2e941366b0e35a0
DenizUzundag <uzundagdeniz@gmail.com>
2021-05-25 00:50:33 +0300

    Data Transfer Object katmanın eklenmesi

---
+++ YSKProje.ToDo.DTO/DTOs/AppUserDtos/AppUserAddDto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YSKProje.ToDo.DTO.DTOs.AppUserDtos
+{
+    public class AppUserAddDto
+    {
+        //[Required(ErrorMessage = "Kullanıcı adı boş geçilemez")]
+        //[Display(Name = "Kullanıcı adı:")]
+        public string UserName { get; set; }
+        //[Required(ErrorMessage = "Kullanıcı adı boş geçilemez")]
+
+        //[Display(Name = "şifre:")]
+        //[DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+        //[Display(Name = "tekrar şifre:")]
+        //[DataType(DataType.Password)]
+
+        //[Compare("Password", ErrorMessage = "Parolalar eşleşmiyor")]
+        public string ConfirmPassword { get; set; }
+
+
+        //[Display(Name = "Email:")]
+        //[EmailAddress(ErrorMessage = "geçersiz email")]
+        //[Required(ErrorMessage = "Email boş geçilemez")]
+        public string Email { get; set; }
+
+
+        //[Display(Name = "Adınız:")]
+
+        //[Required(ErrorMessage = "Name boş geçilemez")]
+        public string Name { get; set; }
+
+        //[Display(Name = "Surname:")]
+
+        //[Required(ErrorMessage = "Surname adı boş geçilemez")]
+        public string Surname { get; set; }
+
+    }
+}
---
+++ YSKProje.ToDo.DTO/DTOs/GorevDtos/GorevListDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YSKProje.ToDo.DTO.DTOs.GorevDtos
+{
+    public class GorevListDto
+    {
+        public int Id { get; set; }
+
+        public string Ad { get; set; }
+        public string Aciklama { get; set; }
+
+        public bool Durum { get; set; }
+        public DateTime OlusturulmaTarihi { get; set; }
+        public int AciliyetId { get; set; }//zorunlu
+        //public Aciliyet Aciliyet { get; set; }
+    }
+}
