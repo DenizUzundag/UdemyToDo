5ae915d6699239d16073b539c75f12a172ee7922
DenizUzundag <uzundagdeniz@gmail.com>
2021-06-01 23:43:27 +0300

    Startup Refactoring

---
+++ YSKProje.ToDo.Business/DiContainer/CustomExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using YSKProje.ToDo.Business.Concrete;
+using YSKProje.ToDo.Business.Interfaces;
+using YSKProje.ToDo.DataAccess.Concrete.EntityFrameworkCore.Repositories;
+using YSKProje.ToDo.DataAccess.Interfaces;
+
+namespace YSKProje.ToDo.Business.DiContainer
+{
+    public static class CustomExtensions
+    {
+        public static void AddContainerWithDepencies(this IServiceCollection services)
+        {
+            services.AddScoped<IGorevService, GorevManager>();//gorevServiceyi görünce gorevManageri örnekle
+            services.AddScoped<IAciliyetService, AciliyetManager>();
+            services.AddScoped<IRaporService, RaporManager>();
+            services.AddScoped<IGorevDal, EfGorevRepository>();
+            services.AddScoped<IRaporDal, EfRaporRepository>();
+            services.AddScoped<IAciliyetDal, EfAciliyetRepository>();
+            services.AddScoped<IAppUserService, AppUserManager>();
+            services.AddScoped<IAppUserDal, EfAppUserRepository>();
+            services.AddScoped<IDosyaService, DosyaManager>();
+            services.AddScoped<IBildirimService, BildirimManager>();
+            services.AddScoped<IBildirimDal, EfBildirimRepository>();
+        }
+    }
+}
--- YSKProje.ToDo.Web/Startup.cs
+++ YSKProje.ToDo.Web/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using YSKProje.ToDo.Business.Concrete;
+using YSKProje.ToDo.Business.DiContainer;
 using YSKProje.ToDo.Business.Interfaces;
 using YSKProje.ToDo.Business.ValidationRules.FluentValidation;
 using YSKProje.ToDo.DataAccess.Concrete.EntityFrameworkCore.Context;
@@ -18,6 +19,7 @@ using YSKProje.ToDo.DTO.DTOs.AppUserDtos;
 using YSKProje.ToDo.DTO.DTOs.GorevDtos;
 using YSKProje.ToDo.DTO.DTOs.RaporDtos;
 using YSKProje.ToDo.Entities.Concrete;
+using YSKProje.ToDo.Web.CustomCollectionExtensions;
 
 namespace YSKProje.ToDo.Web
 {
@@ -27,58 +29,18 @@ namespace YSKProje.ToDo.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            //addScoped :bir tane örnek alýr. ilgili interface ait bir örnek alýr ve her zaman onu veriri
-            //AddTransient: her seferinde yeni bir nesne örneði veriri
-            //AddSingleton: istek kimden gelmiþ nerden gelmiþ önemi yok tek bir nesne örneði üretir.
-            services.AddScoped<IGorevService, GorevManager>();//gorevServiceyi görünce gorevManageri örnekle
-            services.AddScoped<IAciliyetService, AciliyetManager>();
-            services.AddScoped<IRaporService, RaporManager>();
-            services.AddScoped<IGorevDal, EfGorevRepository>();
-            services.AddScoped<IRaporDal, EfRaporRepository>();
-            services.AddScoped<IAciliyetDal, EfAciliyetRepository>();
-            services.AddScoped<IAppUserService, AppUserManager>();
-            services.AddScoped<IAppUserDal, EfAppUserRepository>();
-            services.AddScoped<IDosyaService, DosyaManager>();
-            services.AddScoped<IBildirimService, BildirimManager>();
-            services.AddScoped<IBildirimDal, EfBildirimRepository>();
-            services.AddDbContext<TodoContext>();
-          
-            services.AddIdentity<AppUser, AppRole>(opt=> {
-
-
-                //password validasyon
-                opt.Password.RequireDigit = false;//sayý içerme
-                opt.Password.RequireUppercase = false;//büyük harf
-                opt.Password.RequiredLength = 1;//default 6 biz 1
-                opt.Password.RequireLowercase = false;//küçük harf
-                opt.Password.RequireNonAlphanumeric = false; 
-            }).
             
-           AddEntityFrameworkStores<TodoContext>();
-           
-            services.ConfigureApplicationCookie(opt =>
-            {
-                opt.Cookie.Name = "IsTakipCookie";
-                opt.Cookie.SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Strict;
-                opt.Cookie.HttpOnly = true;
-                opt.ExpireTimeSpan = TimeSpan.FromDays(20);
-                opt.Cookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.SameAsRequest;
-                opt.LoginPath = "/Home/Index";
-            });
+            services.AddContainerWithDepencies();
+            services.AddDbContext<TodoContext>();
+
+            services.AddCustomdIdentity();
+
 
             services.AddAutoMapper(typeof(Startup));
-            services.AddTransient<IValidator<AciliyetAddDto>, AciliyetAddValidator>();
-            services.AddTransient<IValidator<AciliyetUpdateDto>, AciliyetUpdateValidator>();
-            services.AddTransient<IValidator<AppUserAddDto>, AppUserAddValidator>();
-            services.AddTransient<IValidator<AppUserSignInDto>, AppUserSignInValidator>();
-            services.AddTransient<IValidator<GorevAddDto>, GorevAddValidator>();
-            services.AddTransient<IValidator<GorevUpdateDto>, GorevUpdateValidator>();
-            services.AddTransient<IValidator<RaporAddDto>,RaporAddValidator>();
-            services.AddTransient<IValidator<RaporUpdateDto>, RaporUpdateValidator>();
-            services.AddControllersWithViews().AddFluentValidation();
+            services.AddCustomValidator();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env,UserManager<AppUser> userManager,RoleManager<AppRole> roleManager)
         {
             if (env.IsDevelopment())
@@ -101,7 +63,7 @@ namespace YSKProje.ToDo.Web
 
 
                 endpoints.MapControllerRoute(
-                    name:"defaukt",
+                    name:"default",
                     pattern:"{controller=Home}/{action=Index}/{id?}"
                     );
             });
