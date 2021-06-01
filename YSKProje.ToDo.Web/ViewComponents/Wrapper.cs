220692759675f83bc5bb88408e1c2a06c07f4b45
DenizUzundag <uzundagdeniz@gmail.com>
2021-06-01 15:38:53 +0300

    Wrapper View Component

--- YSKProje.ToDo.Web/ViewComponents/Wrapper.cs
+++ YSKProje.ToDo.Web/ViewComponents/Wrapper.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using YSKProje.ToDo.Business.Interfaces;
+using YSKProje.ToDo.DTO.DTOs.AppUserDtos;
 using YSKProje.ToDo.Entities.Concrete;
 using YSKProje.ToDo.Web.Areas.Admin.Models;
 
@@ -15,27 +17,24 @@ namespace YSKProje.ToDo.Web.ViewComponents
         private readonly UserManager<AppUser> _userManager;
 
         private readonly IBildirimService _bildirimService;
-        public Wrapper(UserManager<AppUser> userManager,IBildirimService bildirimService)
+        private readonly IMapper _mapper;
+        public Wrapper(UserManager<AppUser> userManager,IBildirimService bildirimService, IMapper mapper)
         {
             _userManager = userManager;
             _bildirimService = bildirimService;
+            _mapper = mapper;
         }
         public IViewComponentResult Invoke()
         {
-            var user = _userManager.FindByNameAsync(User.Identity.Name).Result;
-            AppUSerListViewModel model = new AppUSerListViewModel();
-
-            model.Id = user.Id;
-            model.Name = user.Name;
-            model.Picture = user.Picture;
-            model.SurName = user.Surname;
-            model.Email = user.Email;
-
-            var bildirimler = _bildirimService.GetirOkunmayanlar(user.Id).Count();
+            var identityUser = _userManager.FindByNameAsync(User.Identity.Name).Result;
+           var model= _mapper.Map<AppUserListDto>(identityUser);
+           
+            var bildirimler = _bildirimService.GetirOkunmayanlar(model.Id).Count();
             ViewBag.BildirimSayisi = bildirimler;
-            var roles = _userManager.GetRolesAsync(user).Result;
+            var roles = _userManager.GetRolesAsync(identityUser).Result;
             if(roles.Contains("Admin"))
             {
+
                 return View(model);
             }
 
